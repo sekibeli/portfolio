66e87f900a4fd6b39e9fb82bc8c2b9b3d952bbf5
Julia Georgiew <java2011@me.com>
2023-06-28 21:24:26 +0200

    aboutMe,myskills,myWork component

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-skills.component.scss']
 })
 export class MySkillsComponent {
-
+skillImages = ['angular.svg','ts.svg','js.svg','html.svg','scrum.svg','fb.svg','git.svg','css.svg','api.svg','material.svg'];
+skillText = ['Angular','Typescript','Javascript','HTML','Scrum','Firebase','Git','CSS','Rest-Api','MaterialDesign'];
 }
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent {
+work:any = {
+  title:  '',
+  dev: '',
+  description: '',
+  img: '',
+  buttonGit:'Github',
+  gitUrl: '',
+  buttonShow:'Test it',
+  testUrl: ''
+}
+
+allWork = [
+  {
+    title:  'Join',
+    dev: 'Angular | Typescript | Javascript | HTML | SCSS',
+    description: 'bla bla bla bla',
+    img: 'join1',
+    buttonGit:'Github',
+    gitUrl: 'https://github.com/sekibeli/join',
+    buttonShow:'Test it',
+    testUrl: 'https://gruppe-5008.developerakademie.net/index.html'
+  },
+  {
+    title:  'La Mama Loca',
+    dev: 'Angular | Typescript | Javascript | HTML | SCSS',
+    description: 'bla bla bla bla',
+    img: 'join1',
+    buttonGit:'Github',
+    gitUrl: 'https://github.com/sekibeli/join',
+    buttonShow:'Test it',
+    testUrl: 'https://gruppe-5008.developerakademie.net/index.html'
+  },
+  {
+    title:  'Ring of Fire',
+    dev: 'Angular | Typescript | Javascript | HTML | SCSS',
+    description: 'bla bla bla bla',
+    img: 'join1',
+    buttonGit:'Github',
+    gitUrl: 'https://github.com/sekibeli/join',
+    buttonShow:'Test it',
+    testUrl: 'https://gruppe-5008.developerakademie.net/index.html'
+  }
+]
+
 
 }
