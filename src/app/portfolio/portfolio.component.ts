b22aa438580b3a3d19abbdbaa1ab8a6d0415e6b1
Julia Georgiew <java2011@me.com>
2023-07-07 20:44:07 +0200

    en translate with ngx-translate

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -21,7 +21,7 @@ allWork = [
   {
     title:  'Join',
     dev: 'Angular | Typescript | Javascript | HTML | SCSS',
-    description: 'Hierbei handelt es sich um einen Taskmanager, der auf dem Prinzip des Kanban-Systems aufbaut. Du hast die Möglichkeit, deine Aufgaben ganz einfach mittels Drag-and-Drop zu organisieren, zu erstellen, zu bearbeiten und zu löschen. Zudem kannst du individuelle Kategorien einrichten und jeder Aufgabe gezielt Mitarbeiter zuweisen.',
+    description: 'Join Beschreibung',
     img: 'join',
     buttonGit:'Github',
     gitUrl: 'https://github.com/sekibeli/join',
@@ -31,7 +31,7 @@ allWork = [
   {
     title:  'La Mama Loca',
     dev: 'Javascript | HTML | CSS',
-    description: 'Ein klassisches Lauf-, spring- und wirf-Spiel, mit objekt-orientiertem Ansatz. Mama ist verrückt und legt sich mit Spinnen und Moskitos an. Hilf ihr den Endboss zu besiegen!',
+    description: 'La Mama Loca Beschreibung',
     img: 'lamamaloca',
     buttonGit:'Github',
     gitUrl: 'https://github.com/sekibeli/join',
@@ -41,7 +41,7 @@ allWork = [
   {
     title:  'Ring of Fire',
     dev: 'Angular | Firebase',
-    description: 'Völlig sinnloses Trink-Kartenspiel, aber sehr hilfreich, um einige Grundprinzipien von Angular und Firebase zu erlernen. Im Multiplayer-Style darf sich jeder betrinken.',
+    description: 'Ring of Fire Beschreibung',
     img: 'ring',
     buttonGit:'Github',
     gitUrl: 'https://github.com/sekibeli/join',
@@ -52,3 +52,6 @@ allWork = [
 
 
 }
+
+
+//'Hierbei handelt es sich um einen Taskmanager, der auf dem Prinzip des Kanban-Systems aufbaut. Du hast die Möglichkeit, deine Aufgaben ganz einfach mittels Drag-and-Drop zu organisieren, zu erstellen, zu bearbeiten und zu löschen. Zudem kannst du individuelle Kategorien einrichten und jeder Aufgabe gezielt Mitarbeiter zuweisen.',
